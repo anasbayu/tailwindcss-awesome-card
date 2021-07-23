d90809d610fb124ca4b2b1379a850d18b03a8af2
anas bayu <anasbayu03@gmail.com>
2021-07-23 22:49:41 +0700

    base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto-condensed': '"Roboto Condensed"'
+      }
+    }
   },
   variants: {
     extend: {},
